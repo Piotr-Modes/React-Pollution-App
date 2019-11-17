ce7f1d2f29bf04c791885e99819d07ea3111fea5
Piotr Modes <piotr.modes@lifeinmobile.com>
2019-11-17 12:18:32 +0100

    add unique url params based on given country name

--- src/apis/WIKIPEDIA.js
+++ src/apis/WIKIPEDIA.js
@@ -2,15 +2,27 @@ import axios from 'axios'
 
 const baseUrl = 'https://en.wikipedia.org/w/api.php'
 
+const countrySpecyficQueries = {
+  Germany: ['state', 'city'],
+  Spain: ['Spain', 'city'],
+  France: ['France', 'department'],
+}
+
 const getListOfWikipediaPagesForGivenCityAndCountry = (cityName, countryName) => {
-  return axios.get(`${baseUrl}?action=query&list=search&format=json&srsearch=${cityName}+${countryName}+city&origin=*`)
+  return axios.get(
+    `${baseUrl}?action=query&list=search&format=json&srsearch=${cityName}${
+      countrySpecyficQueries[countryName] ? `+${countrySpecyficQueries[countryName].join('+')}` : ''
+    }&origin=*`,
+  )
 }
 
 const getCityDescriptionForGivenWikipediaPageId = pageId => {
-  return axios.get(`${baseUrl}?action=query&pageids=${pageId}&format=json&prop=extracts&exintro&origin=*`)
+  return axios.get(
+    `${baseUrl}?action=query&pageids=${pageId}&format=json&prop=extracts&exintro&origin=*`,
+  )
 }
 
 export default {
   getListOfWikipediaPagesForGivenCityAndCountry,
-  getCityDescriptionForGivenWikipediaPageId
+  getCityDescriptionForGivenWikipediaPageId,
 }
