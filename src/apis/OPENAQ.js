7dd3790f5ea7d0e9069326e8f98d31f9ed09d701
Piotr Modes <piotr.modes@lifeinmobile.com>
2020-07-02 12:48:33 +0200

    fix wrong date format error when OPENAQ request

--- src/apis/OPENAQ.js
+++ src/apis/OPENAQ.js
@@ -1,10 +1,11 @@
 import axios from 'axios'
+import { addZ } from '../helperFunctions'
 
 const baseUrl = 'https://api.openaq.org/v1/'
 
 const getLatestPullutionDataForGivenCountry = countryName => {
   const today = new Date()
-  const date = `${today.getFullYear()}-${today.getMonth() + 1}-01`
+  const date = `${today.getFullYear()}-${addZ(today.getMonth() + 1)}-01`
   return axios.get(`${baseUrl}measurements`, {
     params: {
       country: countryName,
--- src/helperFunctions/index.js
+++ src/helperFunctions/index.js
@@ -1,3 +1,5 @@
+export const addZ = n =>  n < 10 ? '0' + n : '' + n
+
 export const capitalize = s => {
   if (typeof s !== 'string') return ''
   return s.charAt(0).toUpperCase() + s.slice(1).toLowerCase()
