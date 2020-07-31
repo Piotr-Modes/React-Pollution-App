4e8078fa39b229ac0f330d4ed727e0ecaf7221c5
Piotr Modes <piotr.modes@lifeinmobile.com>
2020-07-31 12:11:23 +0200

    add-styled-components

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,7 @@ import List from './utilities/List'
 import OfflineMessage from './utilities/OfflineMessage'
 import ErrorMessage from './utilities/ErrorMessage'
 import withLoading from './utilities/withLoading'
+import styled from 'styled-components'
 
 import { getObjectKeyByValue } from '../helperFunctions'
 
